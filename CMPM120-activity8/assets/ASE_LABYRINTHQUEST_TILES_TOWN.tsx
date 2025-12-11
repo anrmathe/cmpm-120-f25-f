e358b844ff77459c783712c6b20bcb26f7465ace
keyan-premji <97200699+keyan-premji@users.noreply.github.com>
2025-12-10 23:58:27 -0800

    removed second game...

--- CMPM120-activity8/assets/ASE_LABYRINTHQUEST_TILES_TOWN.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="ASE_LABYRINTHQUEST_TILES_TOWN" tilewidth="32" tileheight="32" tilecount="961" columns="31">
- <image source="../../../../../Downloads/LABYRINTH QUEST TOWN FREESET/ASE_LABYRINTHQUEST_TILES_TOWN.png" width="1000" height="1000"/>
-</tileset>
